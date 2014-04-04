16c41a7ed38ef7c75debd1ab7d7f2afa0c519bff
Jonas Seibert <jonas@bitbyterstudios.com>
2014-04-04 03:42:58 +0200

    renamed folder

--- src/main/java/com/bitbyterstudios/rewardme/CmdExecutor.java
+++ src/main/java/com/bitbyterstudios/rewardme/CmdExecutor.java
@@ -1,7 +1,6 @@
 package com.bitbyterstudios.rewardme;
 
 import java.util.*;
-import java.util.concurrent.*;
 
 import com.evilmidget38.UUIDFetcher;
 import org.bukkit.Bukkit;
@@ -9,7 +8,6 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.scheduler.BukkitRunnable;
 
 public class CmdExecutor implements CommandExecutor {
 	
--- src/main/java/com/bitbyterstudios/rewardme/RewardMe.java
+++ src/main/java/com/bitbyterstudios/rewardme/RewardMe.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.UUID;
 import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import com.bitbyterstudios.rewardme.listener.BlockListener;
 import com.bitbyterstudios.rewardme.listener.EntityListener;
